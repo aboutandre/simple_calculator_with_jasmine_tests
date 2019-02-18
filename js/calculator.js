deead43a36e6cb9cfc50ec63ae488458b5ec4c90
Andre Lorch <andre.lorch@kuehne-nagel.com>
2019-02-18 15:56:01 +0100

    Add spies

--- js/calculator.js
+++ js/calculator.js
@@ -7,16 +7,13 @@ Calculator.prototype.add = function (number) {
 };
 Calculator.prototype.subtract = function (number) {
     return this.total -= number;
-
 };
 Calculator.prototype.multiply = function (number) {
     return this.total *= number;
-
 };
 Calculator.prototype.divide = function (number) {
     if (number === 0) {
         throw new Error('Cannot divide by zero!');
     }
     return this.total /= number;
-
 };
---
+++ js/main.spec.js
@@ -0,0 +1,42 @@
+describe('main.js', function () {
+    describe('calculate()', function () {
+        it('validate expression when the first number is invalid', function () {
+            spyOn(window, 'updateResult');
+            calculate('a+3');
+            expect(window.updateResult).toHaveBeenCalled();
+            expect(window.updateResult).toHaveBeenCalledWith('Operation not possible');
+        });
+
+        it('validate expression when the second number is invalid', function () {
+            spyOn(window, 'updateResult');
+            calculate('3+a');
+            expect(window.updateResult).toHaveBeenCalled();
+            expect(window.updateResult).toHaveBeenCalledWith('Operation not possible');
+        });
+
+        it('validate expression when the operation is invalid', function () {
+            spyOn(window, 'updateResult');
+            calculate('3_4');
+            expect(window.updateResult).toHaveBeenCalled();
+            expect(window.updateResult).toHaveBeenCalledWith('Operation not possible');
+        });
+
+    });
+
+    describe('updateResult()', function () {
+        beforeAll(function () {
+            element = document.createElement('div');
+            element.setAttribute('id', 'result');
+            document.body.appendChild(element);
+            this.element = element;
+        });
+        afterAll(function () {
+            element = document.getElementById('result');
+            document.body.removeChild(this.element);
+        });
+        it('update result to DOM element', function () {
+            updateResult(5);
+            expect(this.element.innerText).toBe('5');
+        });
+    });
+});
