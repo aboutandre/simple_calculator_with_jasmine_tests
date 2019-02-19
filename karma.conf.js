da751325e2791b16a4fb524d29636506d25a3505
Andre Lorch <andre.lorch@kuehne-nagel.com>
2019-02-19 08:34:25 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
         ],
         plugins: ['karma-jasmine', 'karma-jasmine-matchers', 'karma-chrome-launcher', 'karma-coverage'],
         reporters: ['dots', 'coverage'],
-        browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+        browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
         color: true,
         coverageReporter: {
             dir: 'coverage/',
